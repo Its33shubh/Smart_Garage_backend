663ca5323866fc94e96d3a8c3499845cd4cbd19c
Its33shubh <shubhamkakloter123@gmail.com>
2026-07-08 22:46:07 +0530

    change password api is done of the customer

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -178,12 +178,106 @@ const getProfile = async (req, res) => {
     }
 };
 
+// // Update Customer Profile
+// const updateProfile = async (req, res) => {
+//     try {
 
+//         const { name } = req.body;
 
+//         const customer = await Customer.findById(req.user.id);
+
+//         if (!customer) {
+//             return res.status(404).json({
+//                 error: true,
+//                 success: false,
+//                 message: "Customer not found.",
+//             });
+//         }
+
+//         customer.name = name || customer.name;
+
+//         await customer.save();
+
+//         return res.status(200).json({
+//             error: false,
+//             success: true,
+//             message: "Profile updated successfully.",
+//             data: {
+//                 customerId: customer.customerId,
+//                 name: customer.name,
+//                 role: customer.role,
+//             },
+//         });
+
+//     } catch (error) {
+//         return res.status(500).json({
+//             error: true,
+//             success: false,
+//             message: error.message,
+//         });
+//     }
+// };
+
+// Change Customer Password
+const changePassword = async (req, res) => {
+    try {
+
+        const { currentPassword, newPassword } = req.body;
+
+        if (!currentPassword || !newPassword) {
+            return res.status(400).json({
+                error: true,
+                success: false,
+                message: "Current Password and New Password are required.",
+            });
+        }
+
+        const customer = await Customer.findById(req.user.id);
+
+        if (!customer) {
+            return res.status(404).json({
+                error: true,
+                success: false,
+                message: "Customer not found.",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            currentPassword,
+            customer.password
+        );
+
+        if (!isMatch) {
+            return res.status(401).json({
+                error: true,
+                success: false,
+                message: "Current Password is incorrect.",
+            });
+        }
+
+        customer.password = await bcrypt.hash(newPassword, 10);
+
+        await customer.save();
+
+        return res.status(200).json({
+            error: false,
+            success: true,
+            message: "Password changed successfully.",
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            success: false,
+            message: error.message,
+        });
+    }
+};
 
 
 module.exports = {
     register,
     login,
-    getProfile
+    getProfile,
+    changePassword
 }
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { register,login,getProfile} = require("../controllers/customerController");
+const { register,login,getProfile,changePassword} = require("../controllers/customerController");
 const auth = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/profile",auth(["customer"]), getProfile)
+router.put("/change_password",auth(["customer"]),changePassword);
 
 module.exports = router;
